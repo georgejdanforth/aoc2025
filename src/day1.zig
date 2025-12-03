1ba9c26471abedbfa406866619e74b467c34c651
George Danforth <georgejdanforth@gmail.com>
2025-12-02 22:03:29 -0500

    Runner setup

---
+++ src/day1.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+
+const input = @embedFile("inputs/day1");
+
+pub fn run() void {
+    std.debug.print("Day 1\n", .{});
+}
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,33 @@
 const std = @import("std");
 
+const day1 = @import("./day1.zig");
+
+const input = @embedFile("./inputs/day1");
+
+const Error = error {
+    MissingDay,
+    InvalidDay,
+};
+
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var args_iter = std.process.args();
+    if (!args_iter.skip()) unreachable;
+
+    const day_buf = args_iter.next();
+    if (day_buf == null) {
+        std.debug.print("Missing day\n", .{});
+        return Error.MissingDay;
+    }
+    const day = std.fmt.parseInt(u32, day_buf.?, 10) catch |err| {
+        std.debug.print("Invalid day: {s}", .{day_buf.?});
+        return err;
+    };
+
+    switch (day) {
+        1 => day1.run(),
+        else => {
+            std.debug.print("Invalid day: {d}\n", .{day});
+            return Error.InvalidDay;
+        }
+    }
 }
